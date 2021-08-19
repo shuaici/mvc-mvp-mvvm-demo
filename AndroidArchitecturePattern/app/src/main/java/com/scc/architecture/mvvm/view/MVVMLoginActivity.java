3ddbfb0080ea539b19dde25a6cd3b0c5f8fa9377
shuaici <65161266+gongtianci123@users.noreply.github.com>
2021-08-19 13:50:06 +0800

    Update MVVMLoginActivity.java

--- AndroidArchitecturePattern/app/src/main/java/com/scc/architecture/mvvm/view/MVVMLoginActivity.java
+++ AndroidArchitecturePattern/app/src/main/java/com/scc/architecture/mvvm/view/MVVMLoginActivity.java
@@ -2,13 +2,9 @@ package com.scc.architecture.mvvm.view;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 import com.scc.architecture.R;
 import com.scc.architecture.databinding.ActivityMvvmBinding;
-import com.scc.architecture.mvvm.Callback;
 import com.scc.architecture.mvvm.model.User;
 import com.scc.architecture.mvvm.viewmodel.LoginViewModel;
 
@@ -19,45 +15,24 @@ import androidx.databinding.DataBindingUtil;
 public class MVVMLoginActivity extends AppCompatActivity {
     private LoginViewModel loginVM;
     ActivityMvvmBinding mvvmBinding;
-    private EditText et_account,et_password;
-    private Button btn_login,btn_back;
-    private TextView tv_title;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //返回activity_mvvm的实体对象
         mvvmBinding = DataBindingUtil.setContentView(this, R.layout.activity_mvvm);
-        et_account =findViewById(R.id.et_account);
-        et_password =findViewById(R.id.et_password);
-        btn_login = findViewById(R.id.btn_login);
-        btn_back = findViewById(R.id.btn_back);
-        tv_title = findViewById(R.id.tv_title);
-        tv_title.setText("MVVM");
-
-        loginVM = new LoginViewModel(mvvmBinding);
+        mvvmBinding.setLifecycleOwner(this);
+        loginVM = new LoginViewModel();
+        //创建数据源
         User user = new User( "scc001", "111111");
-        mvvmBinding.setUser(user);//设置et_account：scc001|et_password：111111
-        //第1步：用户点击登录
-        btn_login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                login(et_account.getText().toString(),et_password.getText().toString());
-            }
-        });
-        btn_back.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
-
+        //将数据源交给DataBinding
+        loginVM.setUser(user);
+        //设置et_account：scc001|et_password：111111
+        mvvmBinding.setLoginViewModel(loginVM);
     }
-    private void login(String name,String password) {
-        loginVM.getUser(name,password, new Callback<User>() {
-            @Override
-            public void onCallBack(User user) {
-                mvvmBinding.setUser(user);//同步设置控件
-            }
-        });
+    public void login(View view){
+        loginVM.loginResult();
     }
-
+    public void back(View view){
+        finish();
+    }
 }
